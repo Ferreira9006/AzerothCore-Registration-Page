83e683a12fbcbbf2b139d1b70fc8569a52030213
FlyingArowana <TheSCREWEDSoftware@users.noreply.github.com>
2026-04-21 23:58:20 +0100

    Cleared trailing spaces

--- config.php
+++ config.php
@@ -11,23 +11,23 @@ $slogan = "Create your Account"; // Website Slogan
 $description = "Fill in all the inputs in the form bellow in order to register."; // Website Description
 
 // Check if required extensions are enabled (see GitHub README.md)
-if (!extension_loaded('gmp')) 
+if (!extension_loaded('gmp'))
 {
   die('Please enable the GMP extension in your PHP configuration.');
 }
 
-if (!extension_loaded('mbstring')) 
+if (!extension_loaded('mbstring'))
 {
   die('Please enable the MBString extension in your PHP configuration.');
 }
 
-if (!extension_loaded('pdo') || !extension_loaded('pdo_mysql')) 
+if (!extension_loaded('pdo') || !extension_loaded('pdo_mysql'))
 {
   die('Please enable the PDO-MySQL extension in your PHP configuration.');
 }
 
 // Check if PHP version is 7.4 or higher
-if (version_compare(PHP_VERSION, '7.4.0', '<')) 
+if (version_compare(PHP_VERSION, '7.4.0', '<'))
 {
   die('Please upgrade to PHP 7.4 or higher.');
 }
